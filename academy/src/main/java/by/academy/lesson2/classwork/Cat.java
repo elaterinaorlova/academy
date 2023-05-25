9c0391021aeb1298758627bf0782c3fd24e6eeda
arnijs <arnijs@macbook-air>
2023-05-25 14:14:59 +0300

    cat

---
+++ academy/src/main/java/by/academy/lesson2/classwork/Cat.java
@@ -0,0 +1,42 @@
+package by.academy.lesson2.classwork;
+
+public class Cat {
+
+	String nickname;
+	int age;
+	int money;
+
+	public Cat() {
+		super();
+	}
+
+	public Cat(String nickname) {
+		super();
+		this.nickname = nickname;
+
+	}
+
+	public void grow() {
+		age = age + 1;
+		System.out.println(nickname + " Кот растет");
+	}
+
+	public void sleep() {
+		System.out.println(nickname + " Кот спит ");
+	}
+
+	public void eat() {
+		System.out.println(nickname + " Кот ест ");
+	}
+
+	public void walk() {
+		System.out.println(nickname + " Кот гуляет");
+	}
+
+	@Override
+
+	public String toString() {
+		return "nickname : " + nickname + " age : " + age + " money: " + money;
+	}
+
+}
---
+++ academy/src/main/java/by/academy/lesson4/Task1.java
@@ -0,0 +1,45 @@
+package by.academy.lesson4;
+
+import java.util.Scanner;
+
+//Написать программу, которая в зависимости от номера дня недели выводит название дня недели на экран. Использовать switch/case
+public class Task1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите номер дня недели");
+		int a = sc.nextInt();
+
+		sc.close();
+
+		switch (a) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+
+			break;
+
+		default:
+			System.out.println("There is no such a day");
+			break;
+		}
+	}
+
+}
